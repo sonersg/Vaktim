708f7ba2f131c6cd685d48d1e2491cbbc46f2473
Soner <sonersg@outlook.com>
2025-02-19 17:44:12 +0300

    onLongPress={exitApp}

--- app/(screens)/index.tsx
+++ app/(screens)/index.tsx
@@ -3,16 +3,24 @@ import { Pressable, ScrollView, StyleSheet, View } from 'react-native';
 import Menu from '../../components/Menu';
 import Calendar from '../../components/Calendar';
 import PrayerTimesTable from '../../components/PrayerTimesTable';
+import { BackHandler, Platform } from 'react-native';
 
 export default function Home() {
   const [menuVisible, setmenuVisible] = useState(false);
 
   console.log('index screen');
 
+  const exitApp = () => {
+    if (Platform.OS === 'android') {
+      BackHandler.exitApp();
+    }
+  };
+
   return (
     <Pressable
       style={styles.mainContainer}
       onPress={() => setmenuVisible((prev) => !prev)}
+      onLongPress={exitApp}
     >
       <View>
         <ScrollView>
